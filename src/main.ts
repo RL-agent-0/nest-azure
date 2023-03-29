4c5e3007ebb4a32deb15508b9c7cc5f0ddda42db
unknown <basemzarai@gmail.com>
2023-03-29 21:05:38 +0100

    enabling cors for all

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for http://localhost:4200
   app.enableCors({
-    origin: 'http://localhost:4200'
+    origin: '*'
   });
   await app.listen(3000);
 }
